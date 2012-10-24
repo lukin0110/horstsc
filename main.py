98dfe64adc4b58d742afc645f950bd5ff64af68a
lukin0110 <maarten.huijsmans@gmail.com>
2012-10-25 01:15:34 +0200

    some stuff

--- main.py
+++ main.py
@@ -2,40 +2,19 @@ import logging
 import os
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext.webapp import template
+from django.template import TemplateDoesNotExist
 #os.environ['DJANGO_SETTINGS_MODULE'] = 'horstsc.be.settings'
 
 from google.appengine.dist import use_library
-use_library('django', '1.2')
 
-# Google App Engine imports.
-from google.appengine.ext.webapp import util
-
-# Force Django to reload its settings.
-#from django.conf import settings
-#settings._target = None
-
-#import django.core.handlers.wsgi
-#import django.core.signals
-#import django.db
-#import django.dispatch
-
-# Log errors.
-#import logging
-#def log_exception(*args, **kwds):
-#    logging.exception('Exception in request:')
-#
-#django.dispatch.Signal.connect(
-#    django.core.signals.got_request_exception, log_exception)
-
-# Unregister the rollback event handler.
-#django.dispatch.Signal.disconnect(
-#    django.core.signals.got_request_exception,
-#    django.db._rollback_on_exception)
 
 class HomePage(webapp.RequestHandler):
     def get(self):
-      self.response.headers['Content-Type'] = 'text/plain'
-      self.response.out.write('coming soon, in 2014 ...\n\n')
+        #self.response.headers['Content-Type'] = 'text/plain'
+        #self.response.out.write('coming soon, in 2014 ...\n\n')
+        values = {}
+        self.response.out.write(template.render("templates/index.html", values))
 
 # loading the mappings
 mappings = [
@@ -44,7 +23,6 @@ mappings = [
 
 # initialize the app with the mappings
 application = webapp.WSGIApplication(mappings,debug=True)
-
 
 
 def main():
